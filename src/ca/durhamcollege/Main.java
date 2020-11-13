29f17e252aaefaa964ede3d2c9cc728096a6934e
ahmedrizvi <ahmed.riz117@gmail.com>
2020-11-13 12:29:05 -0500

    basic input and output test

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,24 @@
+/*
+* Author: Ahmed Rizvi
+* Date: 2020-11-13
+ */
+
 package ca.durhamcollege;
 
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String name = null;
+        // Scanner needed for user inputs
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+
+        System.out.printf("You entered: %s", name);
     }
 }
